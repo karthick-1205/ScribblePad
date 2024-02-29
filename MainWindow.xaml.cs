8a3efc117de46eb38e463baa2d0556418abef961
karthickraj <karthickraj.s@metamation.com>
2024-02-29 13:19:11 +0530

    Implement undo,redo and save as text

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace ScribblePad {
+   /// <summary>
+   /// Interaction logic for MainWindow.xaml
+   /// </summary>
+   public partial class MainWindow : Window {
+      Pen pen = new (Brushes.White, 2);
+      PointCollection scribblePoints = new ();
+      List<PointCollection> scribblePointsList = new ();
+      Stack<PointCollection> scribblePointsStack = new ();
+      public MainWindow () => InitializeComponent ();
+
+      protected override void OnRender (DrawingContext dc) {
+         base.OnRender (dc);
+         foreach (PointCollection point in scribblePointsList) {
+            for (int i = 0; i < point.Count - 1; i++) {
+               dc.DrawLine (pen, point[i], point[i + 1]);
+            }
+         }
+         for (int i = 0; i < scribblePoints.Count - 1; i++)
+            dc.DrawLine (pen, scribblePoints[i], scribblePoints[i + 1]);
+      }
+
+      protected override void OnMouseLeftButtonDown (MouseButtonEventArgs e) {
+         if (e.LeftButton == MouseButtonState.Pressed) {
+            Point pt = e.GetPosition (this);
+            scribblePoints.Add (pt);
+         }
+      }
+
+      protected override void OnMouseMove (MouseEventArgs e) {
+         if (e.LeftButton == MouseButtonState.Pressed) {
+            Point pt = e.GetPosition (this);
+            scribblePoints.Add (pt);
+            InvalidateVisual ();
+         }
+      }
+
+      protected override void OnMouseLeftButtonUp (MouseButtonEventArgs e) {
+         if (e.LeftButton == MouseButtonState.Released) {
+            Point pt = e.GetPosition (this);
+            scribblePoints.Add (pt);
+            scribblePointsList.Add (scribblePoints);
+            scribblePoints = new ();
+         }
+      }
+
+      private void Undo_Click (object sender, RoutedEventArgs e) {
+         if (scribblePointsList.Count > 0) {
+            var lastScribble = scribblePointsList.Last ();
+            scribblePointsStack.Push (lastScribble);
+            scribblePointsList.Remove (lastScribble);
+            InvalidateVisual ();
+         }
+      }
+
+      private void Redo_Click (object sender, RoutedEventArgs e) {
+         if (scribblePointsStack.Count > 0) {
+            scribblePointsList.Add (scribblePointsStack.Pop ());
+            InvalidateVisual ();
+         }
+      }
+
+      private void Save_Click (object sender, RoutedEventArgs e) {
+         SaveFileDialog saveText = new ();
+         if (saveText.ShowDialog () == true) {
+            using StreamWriter sw = new (saveText.FileName);
+            foreach (var ptList in scribblePointsList) {
+               foreach (var pt in ptList) { sw.WriteLine (pt.ToString ()); }
+               sw.WriteLine ("end");
+            }
+         }
+      }
+   }
+}
